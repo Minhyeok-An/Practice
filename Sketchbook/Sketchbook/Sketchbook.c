55cf7dced6700b99bb29329753def0ba2f9b3efa
Minhyeok-An <mh010408@gmail.com>
2020-12-25 15:54:39 +0900

    Inf Plus Calculator

--- Sketchbook/Sketchbook/Sketchbook.c
+++ Sketchbook/Sketchbook/Sketchbook.c
@@ -2,20 +2,13 @@
 
 int main()
 {
-	int i, k;
-
-	for (k = 2;k <= 9;k++)
+	int a, b;
+	
+	for (; ; )
 	{
-		printf("---- %d단 ----\t", k);
-	}
-	printf("\n");
+		printf("더할 두 수를 입력 (멈추려면 Ctrl+C) : ");
+		scanf("%d %d", &a, &b);
 
-	for (i = 1; i <= 9; i++)
-	{
-		for (k = 2; k <= 9; k++)
-		{
-			printf("%02d X %02d = %2d\t", k, i, k * i);
-		}
-		printf("\n");
+		printf("%d + %d = %d \n", a, b, a + b);
 	}
 }
