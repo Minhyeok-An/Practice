6122d4509389938b701a325964185e27f0ce076a
Minhyeok-An <mh010408@gmail.com>
2020-12-26 17:49:33 +0900

    stack 1

--- Sketchbook/Sketchbook/Sketchbook.c
+++ Sketchbook/Sketchbook/Sketchbook.c
@@ -1,20 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-	int i, k;
+	char stack[11];
+	int top = 0;
 
-	for (i = 2; i <=9 ; i++)
-		printf(" #제%d단#", i);
+	char carName = 'A';
+	int select = 0;
 
-	printf("\n\n");
-
-	for (i = 1; i <= 9; i++)
+	while (select != 3)
 	{
-		for (k = 2; k <= 9; k++)
+		printf("<1> 자동차 넣기 <2> 자동차 빼기 <3> 끝 :");
+		scanf("%d", &select);
+
+		switch (select)
 		{
-			printf("%2dX%2d=%2d", k, i, k * i);
+		case 1:
+			if (top >= 10)
+			{
+				printf("터널이 꽉 차서 차가 못 들어감\n");
+			}
+			else
+			{
+				stack[top] = carName++;
+				printf("%c 자동차가 터널에 들어감\n", stack[top]);
+				top++;
+			}
+			break;
+
+		case 2:
+			if (top <= 0)
+			{
+				printf("터널에 차가 없음\n");
+			}
+			else
+			{
+				top--;
+				printf("%c 자동차가 터널을 빠져나감\n", stack[top]);
+				stack[top] = ' ';
+			}
+			break;
+
+		case 3:
+			stack[top] = '\0';
+			printf("현재 터널에 %d대가 있음.\n프로그램을 종료합니다.", top);
+			printf("%s", stack);
+			break;
+
+		default:
+			printf("잘못 입력했습니다.\n");
 		}
-		printf("\n");
 	}
 }
