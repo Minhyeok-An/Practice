ae62edbe6bb2343fd150f1342a0cbb54603a35bd
Minhyeok-An <mh010408@gmail.com>
2020-12-29 00:22:18 +0900

    1229

--- Sketchbook/Sketchbook/Sketchbook.c
+++ Sketchbook/Sketchbook/Sketchbook.c
@@ -3,7 +3,27 @@
 
 int main()
 {
-	int a[3] = { 10,20,30 };
+	int s[10] = { 2,3,5,7,8,9,0,1,4,6 };
+	int i, k, tmp;
 
-	printf("%d\n%d\n%d\n%d", &a[0], &a[1], &a[2], a);
+	int* p;
+	p = s;
+
+	for (i = 0;i < 9;i++)
+	{
+		for (k = i;k < 9;k++)
+		{
+			if (*(p + i) > *(p + k))
+			{
+				tmp = *(p + i);
+				*(p + i) = *(p + k);
+				*(p + k) = tmp;
+			}
+		}
+	}
+	
+	for (i = 0;i < 10;i++)
+	{
+		printf("%d", *(p + 1));
+	}
 }
