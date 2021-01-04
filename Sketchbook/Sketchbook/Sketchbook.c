bfae8e2cecdb4d1fcadb9b84eef0c93987b1b124
Minhyeok-An <mh010408@gmail.com>
2021-01-05 00:00:14 +0900

    0104

--- Sketchbook/Sketchbook/Sketchbook.c
+++ Sketchbook/Sketchbook/Sketchbook.c
@@ -2,40 +2,17 @@
 
 int main()
 {
-	char a;
-	int b, plus;
+	int a, b, max, i;
 
-	scanf("%c %d", &a, &b);
+	scanf("%d %d", &a, &b);
 
-	if (a >= 'A' && a <= 'Z')
-	{
-		plus = b % ('Z' - 'A' + 1);
-		a = a + plus;
-		if (a >= 'A' && a <= 'Z')
-		{
-			printf("%c", a);
-		}
-		else
-		{
-			printf("%c", a - ('Z' - 'A' + 1));
-		}
-	}
-	else if (a >= 'a' && a <= 'z')
-	{
-		plus = b % ('z' - 'a' + 1);
-		a = a + plus;
-		if (a >= 'a' && a <= 'z')
-		{
-			printf("%c", a);
-		}
-		else
-		{
-			printf("%c", a - ('z' - 'a' + 1));
-		}
-	}
-	else
-	{
-		printf("%c", a);
-	}
+	max = a < b ? b : a;
+	i = a < b ? a : b;
 
+	while (i != 0)
+	{
+		i = max % i;
+		
+	}
+	printf("%d", max);
 }
