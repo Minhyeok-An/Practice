9b6bb442bbe5bf98633dec588f4edf54a5b41856
Minhyeok-An <mh010408@gmail.com>
2021-01-06 13:38:24 +0900

    snake

--- Sketchbook/Sketchbook/Sketchbook.c
+++ Sketchbook/Sketchbook/Sketchbook.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-
 int main()
 {
-	int a, b, max, i;
+	int n, i, j;
+	scanf("%d", &n);
 
-	scanf("%d %d", &a, &b);
-
-	max = a < b ? b : a;
-	i = a < b ? a : b;
-
-	while (i != 0)
+	for (i = 1;i <= n;i++)
 	{
-		i = max % i;
-		
+		if (i % 2 == 1)
+		{
+			for (j = (i - 1) * n + 1;j <= i * n;j++)
+			{
+				printf(" %d", j);
+			}
+			printf("\n");
+		}
+		if (i % 2 == 0)
+		{
+			for (j = i * n;j >= (i - 1) * n + 1;j--)
+			{
+				printf(" %d", j);
+			}
+			printf("\n");
+		}
+
 	}
-	printf("%d", max);
 }
